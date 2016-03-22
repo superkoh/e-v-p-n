23f44f8898efd31cedc38a7dcc7657f903a4ce20
王乐天 <koh.718@gmail.com>
2016-03-22 14:49:59 +0800

    fix bug

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
 $config = [
-    'ok': 0,
-    'obj': [
-        'servers': [
+    'ok'=> 0,
+    'obj'=> [
+        'servers'=> [
             [
-                'name': '东京',
-                'ip': '52.69.16.186'
+                'name'=> '东京',
+                'ip'=> '52.69.16.186'
             ],
             [
-                'name': '加利福尼亚',
-                'ip': '104.131.143.69'
+                'name'=> '加利福尼亚',
+                'ip'=> '104.131.143.69'
             ]
         ],
-        'secret': 'superkoh'
+        'secret'=> 'superkoh'
     ]
 ];
 header('Content-Type: application/json; charset=utf-8');
